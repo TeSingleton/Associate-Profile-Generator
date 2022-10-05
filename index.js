190e1cf50fe6d87db074b5cd8a35339dadbf6293
Te <worksingletonmcdaniel@gmail.com>
2022-10-04 19:56:16 -0700

    changes to git ignore

---
+++ index.js
@@ -0,0 +1,34 @@
+// require inquirer
+
+// Create an empty array List to store employee objects
+
+// gather Manager data
+
+        // inquirer .prompt()
+
+          // Then build a Manager object
+
+//Gather Engineer data
+    
+    // inquirer .prompt()
+
+    // Then build a Engineer object
+
+//Gather Intern data
+
+    // inquirer .prompt()
+
+    // Then build a Intern object
+
+
+
+//Ask for "which team member they want to add next" or "are they done?"
+
+       // inquirer .prompt()
+
+       //    Then deciding which function then call
+
+      // generate the HTML and write it to a file 
+
+
+
