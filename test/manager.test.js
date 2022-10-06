16633930e0a26f5f5f9fa795b816f529ac08d23e
Te <worksingletonmcdaniel@gmail.com>
2022-10-05 23:00:36 -0700

    completed manager test

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,29 +1,63 @@
 const Manager = require("../lib/manager");
 
-describe("Intern",()=>{
-    describe("gerRole",()=>{
-        it("should pass through the entered name when getName is called",()=>{
-            const manager = new Manager(
-               " Svante",
+describe("Intern", () => {
+  describe("gerRole", () => {
+    it("should pass through the entered name when getName is called", () => {
+      const manager = new Manager(
+        " Svante",
         "111011",
         "worksingletonmcdaniel@gmail.com"
-            );
-             const managerRole = manager.getRole();
+      );
+      const managerRole = manager.getRole();
 
-             expect(managerRole).toEqual("Manager");
-        })
-    })
+      expect(managerRole).toEqual("Manager");
+    });
+  });
 
-    describe("getName",()=>{
-        it("should pass the ID when getID is called ", ()=> {
-            const manager = new Manager(
-                " Svante",
-         "111011",
-         "worksingletonmcdaniel@gmail.com"
-             );
-            const managerName = manager.getName();
+  describe("getName", () => {
+    it("should pass the ID when getID is called ", () => {
+      const manager = new Manager(
+        " Svante",
+        "111011",
+        "worksingletonmcdaniel@gmail.com"
+      );
+      const managerName = manager.getName();
 
-            expect(managerName).toEqual("Svante")
-        })
-    })
-})
+      expect(managerName).toEqual("Svante");
+    });
+  });
+
+  describe("getID", () => {
+    it("should pass through the stored ID when the getID is called", () => {
+      const manager = new Manager(
+        "Svante",
+        "111011",
+        "worksingletonmcdaniel@gmail.com"
+      );
+
+      const managerID = manager.getID();
+
+      expect(managerID).toEqual("111011");
+    });
+  });
+
+  describe("getEmail", () => {
+    it("should pass through the stored email when the getEmail method is called ", () => {
+      const managerEmail = manager.getEmail();
+
+      expect(managerEmail).toEqual("111011");
+    });
+  });
+  describe("getOfficeNumber", () => {
+    it("should pass through the stored email when the getOfficeNumber method is called", () => {
+      const manager = new Manager(
+        "Svante",
+        "111011",
+        "worksingletonmcdaniel@gmail.com"
+      );
+      const managerPhone = manager.getOfficeNumber();
+
+      expect(managerPhone).toEqual("281-330-8004");
+    });
+  });
+});
