888bc34605d2cb7f00eca8e2c97fcd239eb985c9
Te <worksingletonmcdaniel@gmail.com>
2022-10-06 11:36:16 -0700

    updating engineer.js

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,69 +1,68 @@
+const Engineer = require("../lib/employee");
 
+describe("Engineer", () => {
+  describe("getRole", () => {
+    it("should pass thriugh the name inputted when get name is called", () => {
+      const engineer = new Engineer(
+        "Svante",
+        "111011",
+        "worksingletonmcdaniel@gmail.com"
+      );
+      const employeeRoll = engineer.getRole();
 
-const Engineer = require("../lib/employee")
+      expect(employeeRoll).toEqual("Engineer");
+    });
+  });
 
-describe("Engineer",()=>{
+  describe("getName", () => {
+    it("should pass through the ID when getID is called", () => {
+      const engineer = new Engineer(
+        "Svante",
+        "111011",
+        "worksingletonmcdaniel@gmail.com"
+      );
 
-    describe("getRole",()=>{
-        it("should pass thriugh the name inputted when get name is called",()=>{
-            const engineer = new Engineer(
-                "Svante",
-                "111011",
-                "worksingletonmcdaniel@gmail.com"
-              )
-              const employeeRoll = engineer.getRole();
+      const employeeName = engineer.getName();
 
-              expect(employeeRoll).toEqual("Engineer")
-        })
-    })
+      expect(employeeName).toEqual("Svante");
+    });
+  });
 
-    describe("getName",()=>{
-        it("should pass through the ID when getID is called",()=>{
-            const engineer = new Engineer(
-                "Svante",
-                "111011",
-                "worksingletonmcdaniel@gmail.com"
-              )
+  describe("getID", () => {
+    const engineer = new Engineer(
+      "Svante",
+      "111011",
+      "worksingletonmcdaniel@gmail.com"
+    );
 
-              const employeeName = engineer.getName()
+    const engineerID = engineer.getID();
 
-                expect(employeeName).toEqual("Svante")
-        })
-    })
+    expect(engineerID).toEqual("111011");
+  });
 
-    describe("getID",()=>{
-        const engineer = new Engineer(
-            "Svante",
-            "111011",
-            "worksingletonmcdaniel@gmail.com"
-          )
+  describe("getEmail", () => {
+    it("should pass through the stored email when the method getEmail is called ", () => {
+      const engineer = new Engineer(
+        "Svante",
+        "111011",
+        "worksingletonmcdaniel@gmail.com"
+      );
+      const engineerEmail = engineer.getEmail();
 
-          const engineerID=engineer.getID();
+      expect(engineerEmail).toEqual("worksingletonmcdaniel@gmail.com");
+    });
+  });
 
-          expect(engineerID).toEqual("111011")
+  describe("getGithub", () => {
+    it("should pass through the stored email when the method getGithub is called", () => {
+      const engineer = new Engineer(
+        "Svante",
+        "111011",
+        "worksingletonmcdaniel@gmail.com"
+      );
+      const engineerGhub = engineer.getGithub();
 
-    })
-
-    describe("getEmail",()=>{
-        it("should pass through the stored email when the method getEmail is called ",()=>{
-            const engineer =new Engineer(  "Svante",
-            "111011",
-            "worksingletonmcdaniel@gmail.com")
-            const engineerEmail = engineer.getEmail();
-
-            expect(engineerEmail).toEqual("worksingletonmcdaniel@gmail.com")
-        })
-    })
-
-    describe("getGithub",()=>{
-        it("should pass through the stored email when the method getGithub is called",()=>{
-            const engineer = new Engineer("Svante",
-            "111011",
-            "worksingletonmcdaniel@gmail.com")
-            const engineerGhub =engineer.getGithub();
-
-            expect(engineerGhub).toEqual("worksingletonmcdaniel@gmail.com")
-        })
-    })
-
-})
+      expect(engineerGhub).toEqual("worksingletonmcdaniel@gmail.com");
+    });
+  });
+});
