99d745ef11f51afd34e2c5d3e90c820be9a6a78a
Te <worksingletonmcdaniel@gmail.com>
2022-10-08 20:33:41 -0700

    added renderTitle function to generate HTML

--- src/generateTeamHTML.js
+++ src/generateTeamHTML.js
@@ -1,9 +1,63 @@
-function templateHTML(employees){
-    // use template literal to generate team member profiles
+var teamCardsArray = [];
+
+function templateHTML(employees) {
+  renderTeamCards(employees);
+
+  teamCardsArray = teamCardsArray.join(" ");
+  return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+        />
+        <link rel="stylesheet" href="./style.css" />
     
-    // create a function to get employees array
+        <title>Employee Profiles</title>
+      </head>
+      <body>
+        <center>
+          <header id="header" class="notification is-primary">
+            Employee Profiles
+          </header>
+        </center>
+        <!-- todo css and complete html -->
+        <main>
+        ${teamCardsArray}
+        </main>
+          <center>
+      <footer id="footer" class="notification is-primary"> </footer>
+    </center>
+    </body>
+  </html>`;
+  // use template literal to generate team member profiles
+  // create a function to get employees array
+  // create an array variable
+  // create template literal for the employees to be appended to
+}
 
-    // create an array variable
+function renderTitle() {
+  //  if employees.getRole === Manager
+  // return  phone number
+  if (employees.getRole() === "Manager") {
+    return `Office Number: <a href= "tel:${employees.getOfficeNumber()}" >${employees.getOfficeNumber()}</a>`;
+  } //if employee.getRole === Engineer
+  // return github
+  else if (employees.getRole() === "Engineer") {
+    return `GitHub <a "href=https://github.com/${employees.getGethub()}" target="_blank" >${employees.getGithub()}</a>`;
+  } // if employee,getRole === intern
+  // return school
+  else if (employees.getRole() === "Intern") {
+    return `School:${employees.getSchool}`;
+  }
+}
 
-    // create template literal for the employees to be appended to
+function renderTeamCards(employees) {
+  // loop through the employees array.length
+  // initialize a variable with a template literal
+  // then push
 }
